101c91fd5dda3cc538a0c59c272a7fe2e7efb832
Olipolite <oliver.oquist3d@gmail.com>
2022-10-15 10:44:06 +0200

    Finished fizzBuzz and booleanToWord

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("Returns Yes if boolean is passed true", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+  });
+
+  it("Returns No if boolean is passed false", () => {
+    expect(booleanToWord(false)).toBe("No");
+  });
 });
