e95167ed7f24e733358db0da3b62dc8b4f0a7c24
Olipolite <oliver.oquist3d@gmail.com>
2022-10-15 12:35:24 +0200

    Finished numberToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,6 @@
-const numberToReversedDigits = (number) => {};
-
+const numberToReversedDigits = (number) => {
+  const numArray = Array.from(String(number), Number);
+  numArray.reverse();
+  return numArray;
+};
 module.exports = numberToReversedDigits;
