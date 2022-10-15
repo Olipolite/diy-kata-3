e494639a5d31d35992309163a6c72f86e5efc94b
Olipolite <oliver.oquist3d@gmail.com>
2022-10-15 14:18:37 +0200

    Finished humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,21 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  let array = [0, 0, 0];
+  for (let i = 0; i < number; i++) {
+    if (i === 0) {
+      array[0] += 1; //humanYears
+      array[1] += 15; //catYears
+      array[2] += 15; //dogYears
+    } else if (i === 1) {
+      array[0] += 1;
+      array[1] += 9;
+      array[2] += 9;
+    } else if (i <= number) {
+      array[0] += 1;
+      array[1] += 4;
+      array[2] += 5;
+    }
+  }
+  return array;
+};
 
 module.exports = humanCatDogYears;
