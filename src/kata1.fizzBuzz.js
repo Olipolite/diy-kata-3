bc661c34375f18751cecb1ed143f1c6242b5fbec
Olipolite <oliver.oquist3d@gmail.com>
2022-10-15 10:45:39 +0200

    Finished fizzBuzz and booleanToWord

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,12 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else if(number % 3 === 0) {
+    return "Fizz";
+  }
+  return number;
+};
 
 module.exports = fizzBuzz;
