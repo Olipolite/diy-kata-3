47417868f5d5fdea76bb5930aba2c7517237bb70
Olipolite <oliver.oquist3d@gmail.com>
2022-10-16 22:51:19 +0200

    This did my head in wtf joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,21 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  let result = "";
+  const comma = ", ";
+  const end = " & ";
+  while (namesObj.length) {
+    result += namesObj.shift().name;
+    if (namesObj.length == 1) {
+      result += end;
+    } else if (namesObj.length > 1) {
+      result += comma;
+    }
+  }
+  return result;
+};
 
 module.exports = joinNames;
+
+// let simpsons = namesObj;
+// simpsons = simpsons.map((names) => names.name);
+// simpsons.splice(-1, 0, "&");
+// return console.log(simpsons);
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,40 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Bart",
+        },
+        {
+          name: "Lisa",
+        },
+        {
+          name: "Maggie",
+        },
+      ])
+    ).toEqual("Bart, Lisa & Maggie");
+  });
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Bart",
+        },
+        {
+          name: "Lisa",
+        },
+        {
+          name: "Maggie",
+        },
+        {
+          name: "Cringe",
+        },
+        {
+          name: "Stjärt",
+        },
+      ])
+    ).toEqual("Bart, Lisa, Maggie, Cringe & Stjärt");
+  });
 });
