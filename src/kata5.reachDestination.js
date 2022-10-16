a7d6117b9101bc541bbd4b06fa8bf961fab78e5f
Olipolite <oliver.oquist3d@gmail.com>
2022-10-16 19:37:49 +0200

    Finished reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  let equation = distance / speed;
+  equation = Math.ceil(equation * 2) / 2;
+  return `I should be there in ${equation} hours.`;
+};
 
 module.exports = reachDestination;
