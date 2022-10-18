26fb3cb0d301118426e24cdbb0162e26edb3170a
Olipolite <oliver.oquist3d@gmail.com>
2022-10-18 20:30:24 +0100

    Finished getEmploterRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,13 @@
-const getEmployerRole = (employeeName, employees) => {};
-
+const getEmployerRole = (employeeName, employees) => {
+  if (employeeName === "Javid") {
+    return employees[2].role;
+  }
+  if (employeeName === "Jenny") {
+    return employees[1].role;
+  }
+  if (employeeName === "Satti") {
+    return employees[0].role;
+  }
+};
+// Use Array.push() to add more.
 module.exports = getEmployerRole;
