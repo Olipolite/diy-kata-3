eb1047a39c5f761df3567157e84df9fcff5b5353
Olipolite <oliver.oquist3d@gmail.com>
2022-10-18 20:29:52 +0100

    Finished getEmployerRole, might not be best solution as if we add more people other than array.push, might break code.

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,30 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  const employees = [
+    {
+      name: "Satti",
+
+      role: "Developer",
+    },
+    {
+      name: "Jenny",
+
+      role: "Sales Associate",
+    },
+    {
+      name: "Javid",
+
+      role: "Human Recommended Reading Assistant",
+    },
+  ];
+
+  it("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Javid", employees)).toEqual(
+      "Human Recommended Reading Assistant"
+    );
+  });
+  it("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Jenny", employees)).toEqual("Sales Associate");
+  });
 });
